306d70913628d625397fb06d29f91c55763f49d7
Cliff Yapp <238416+starseeker@users.noreply.github.com>
2019-11-16 11:06:22 -0500

    Go with the CMake build (remove autotools) and simplify - not worried about Windows (currently).  Consolidate thanks into authors file.

--- distribute/win/svg2ico.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-#
-size=64
-inkscape -z -e AutoTraceIcon.$size.png -w $size -h $size ../mac/icns/AutoTraceIcon.svg
-convert AutoTraceIcon.$size.png autotrace.ico
-rm -f AutoTraceIcon.$size.png
-
-
